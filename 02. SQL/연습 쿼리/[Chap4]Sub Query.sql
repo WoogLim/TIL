f30fc82276cf7297289e47bba46ce0debb95a245
WoogLim <elegy74@gmail.com>
2021-08-31 23:41:56 +0900

    08 31

--- 02. SQL/연습 쿼리/[Chap4]Sub Query.sql
+++ 02. SQL/연습 쿼리/[Chap4]Sub Query.sql
@@ -1,24 +1,37 @@
-SELECT T0.CUS_ID, T0.CUS_NM, T0.BASE_YM, NVL(T2.ORD_CNT,0) ORD_CNT
-  FROM ( SELECT T1.CUS_ID
-                ,T1.CUS_NM
-                ,T4.BASE_YM
-           FROM M_CUS T1
-                ,(
-                SELECT TO_CHAR(ADD_MONTHS(TO_DATE('20170201','YYYYMMDD'), ROWNUM - 1),'YYYYMM') BASE_YM
-                  FROM DUAL
-                  CONNECT BY ROWNUM <= 3
-                  ) T4
-          WHERE T1.CUS_ID IN('CUS_0003','CUS_0004')) T0
-         ,(
-         SELECT A.CUS_ID
-                ,TO_CHAR(A.ORD_DT,'YYYYMM') ORD_YM
-                ,COUNT(*) ORD_CNT
-           FROM T_ORD A
-          WHERE A.CUS_ID IN('CUS_0003','CUS_0004')
-            AND A.ORD_DT >= TO_DATE('20170201','YYYYMMDD')
-            AND A.ORD_DT < TO_DATE('20170501','YYYYMMDD')
-          GROUP BY A.CUS_ID
-                   , TO_CHAR(A.ORD_DT,'YYYYMM')) T2
- WHERE T0.CUS_ID = T2.CUS_ID(+)
-   AND T0.BASE_YM = T2.ORD_YM(+)
- ORDER BY T0.CUS_ID, T0.BASE_YM;
+-- 17년 8월 총 주문금액 구하기 - SELECT절 단독 서브쿼리
+SELECT  TO_CHAR(T1.ORD_DT, 'YYYYMMDD') ORD_YMD
+        ,SUM(T1.ORD_AMT) ORD_AMT
+        ,(
+        SELECT  SUM(A.ORD_AMT)
+        FROM    T_ORD A
+        WHERE   A.ORD_DT >= TO_DATE('20170801','YYYYMMDD')
+        AND     A.ORD_DT < TO_DATE('20170901','YYYYMMDD')
+        ) TOTAL_ORD_AMT
+FROM    T_ORD T1
+WHERE   T1.ORD_DT >= TO_DATE('20170801','YYYYMMDD')
+AND     T1.ORD_DT < TO_DATE('20170901','YYYYMMDD')
+GROUP BY TO_CHAR(T1.ORD_DT, 'YYYYMMDD');
+
+-- 17년 8월 총 주문금액, 주문일자의 주문금액비율 구하기 - SELECT절 단독 서브쿼리
+-- 주문금액 비율 = 주문일자별 주문금액(ORD_AMT) / 17년8월 주문 총 금액(TOTAL_ORD_AMT) * 100.00
+SELECT  TO_CHAR(T1.ORD_DT, 'YYYYMMDD') ORD_YMD
+        ,SUM(T1.ORD_AMT) ORD_AMT
+        ,(
+        SELECT  SUM(A.ORD_AMT)
+        FROM    T_ORD A
+        WHERE   A.ORD_DT >= TO_DATE('20170801','YYYYMMDD')
+        AND     A.ORD_DT < TO_DATE('20170901','YYYYMMDD')
+        ) TOTAL_ORD_AMT
+        ,ROUND(
+        SUM(T1.ORD_AMT) / (
+            SELECT  SUM(A.ORD_AMT)
+            FROM    T_ORD A
+            WHERE   A.ORD_DT >= TO_DATE('20170801','YYYYMMDD')
+            AND     A.ORD_DT < TO_DATE('20170901','YYYYMMDD')
+        ) * 100, 2) ORD_AMT_RT
+FROM    T_ORD T1
+WHERE   T1.ORD_DT >= TO_DATE('20170801','YYYYMMDD')
+AND     T1.ORD_DT < TO_DATE('20170901','YYYYMMDD')
+GROUP BY TO_CHAR(T1.ORD_DT, 'YYYYMMDD');
+
+-- 인라인-뷰를 사용해 반복 서브쿼리를 제거하는 방법
